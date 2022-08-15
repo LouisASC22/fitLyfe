4ab2ae948462b5ce48fdaf3c69fa296d14643de5
Glitch (glitch-hello-website) <none>
2022-08-15 15:07:37 +0000

    🛋💴 Checkpoint ./style.css:47040429/1020 ./index.html:47040396/128:47040429/256:47042280/240:47102482/151 ./script.js:47040429/161

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 /* when the cursor goes over the nav, it opens */
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("nav_bar").style.width = "250px";
   document.getElementById("main_div").style.marginLeft = "250px";
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #818181;
+  display: block;
+  transition: 0.3s;
 }
 
 /* closes the nav, set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("nav_bar").style.width = "0";
   document.getElementById("main_div").style.marginLeft = "0";
 }
