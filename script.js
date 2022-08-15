59c829ccb7514f2924a1a70fa9677cb72f350fbc
Glitch (glitch-hello-website) <none>
2022-08-15 15:30:56 +0000

    🏹🎯 Checkpoint ./index.html:47040396/9:47040429/59 ./script.js:47040429/342 ./style.css:47040429/822

--- script.js
+++ script.js
@@ -2,18 +2,18 @@
 function openNav() {
   document.getElementById("nav_bar").style.width = "250px";
   document.getElementById("main_div").style.marginLeft = "250px";
-  document.getElementById("nav_bar").style.padding = "8px 8px 8px 32px";
-  document.getElementById("nav_bar").style.fontSize = "25px";
-  document.getElementById("nav_bar").style.color = "#818181";
-  document.getElementById("nav_bar").style.display = "block";
+  document.querySelector("a").style.padding = "8px 8px 8px 32px";
+  document.querySelector("a").style.fontSize = "25px";
+  document.querySelector("a").style.color = "#818181";
+  document.querySelector("a").style.display = "block";
 }
 
 /* closes the nav, set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("nav_bar").style.width = "0";
   document.getElementById("main_div").style.marginLeft = "0";
-  document.getElementById("nav_bar").style.padding = "25px 0";
-  document.getElementById("nav_bar").style.fontSize = "16px";
-  document.getElementById("nav_bar").style.color = "currentColor";
-  document.getElementById("nav_bar").style.display = "inline-block";
+  document.querySelector("a").style.padding = "25px 0";
+  document.querySelector("a").style.fontSize = "16px";
+  document.querySelector("a").style.color = "currentColor";
+  document.querySelector("a").style.display = "inline-block";
 }
